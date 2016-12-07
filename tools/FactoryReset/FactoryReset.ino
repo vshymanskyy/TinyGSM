0977e2672db71c8aeffa58217eb3a5c3e5ae4eff
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-12-07 16:04:12 +0200

    Update factory reset

--- tools/FactoryReset/FactoryReset.ino
+++ tools/FactoryReset/FactoryReset.ino
@@ -9,30 +9,46 @@
  *
  **************************************************************/
 
-#include <TinyGsmClient.h>
-#include <StreamDebugger.h>
+// Set serial for debug console (to the Serial Monitor, speed 115200)
+#define SerialMonitor Serial
 
+// Set serial for AT commands (to the module)
 // Use Hardware Serial on Mega, Leonardo, Micro
-#define GsmSerial Serial1
+#define SerialAT Serial1
 
 // or Software Serial on Uno, Nano
-//#include <SoftwareSerial.h>
-//SoftwareSerial GsmSerial(2, 3); // RX, TX
+//#include <SoftwareSerialMonitor.h>
+//SoftwareSerial SerialAT(2, 3); // RX, TX
 
-StreamDebugger DebugSerial(GsmSerial, Serial);
-TinyGsmClient gsm(DebugSerial);
+#include <TinyGsmClient.h>
+#include <StreamDebugger.h>
+StreamDebugger DebugAT(SerialAT, SerialMonitor);
+TinyGsmClient gsm(DebugAT);
 
 void setup() {
   // Set console baud rate
-  Serial.begin(115200);
+  SerialMonitor.begin(115200);
   delay(10);
 
   // Set GSM module baud rate
-  GsmSerial.begin(115200);
+  SerialAT.begin(115200);
   delay(3000);
 
-  // Return to factory configuration
-  gsm.factoryDefault();
+  if (!gsm.begin()) {
+    SerialMonitor.println(F("***********************************************************"));
+    SerialMonitor.println(F(" Cannot initialize module!"));
+    SerialMonitor.println(F("   Use File -> Examples -> TinyGSM -> tools -> AT_Debug"));
+    SerialMonitor.println(F("   to find correct configuration"));
+    SerialMonitor.println(F("***********************************************************"));
+    return;
+  }
+
+  bool ret = gsm.factoryDefault();
+
+  SerialMonitor.println(F("***********************************************************"));
+  SerialMonitor.print  (F(" Return settings to Factory Defaults: "));
+  SerialMonitor.println((ret) ? "OK" : "FAIL");
+  SerialMonitor.println(F("***********************************************************"));
 }
 
 void loop() {
