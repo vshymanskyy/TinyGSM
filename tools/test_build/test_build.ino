b5dace330bd21b9b1da013d05d3db72990fde3eb
SRGDamia1 <sdamiano@stroudcenter.org>
2017-06-08 13:16:22 -0400

    Fixed indentation error

--- tools/test_build/test_build.ino
+++ tools/test_build/test_build.ino
@@ -4,7 +4,7 @@
  *
  **************************************************************/
 
- #define TINY_GSM_MODEM_SIM800
+ // #define TINY_GSM_MODEM_SIM800
  //#define TINY_GSM_MODEM_A6
  //#define TINY_GSM_MODEM_M590
  //#define TINY_GSM_MODEM_ESP8266
