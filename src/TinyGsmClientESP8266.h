6a04def5f3be9f51a7cfc9bfa7fcc2ea04e330e5
SRGDamia1 <sdamiano@stroudcenter.org>
2017-10-26 16:15:37 -0400

    Removed extra wait for 'connect' b/c it is before the ok

--- src/TinyGsmClientESP8266.h
+++ src/TinyGsmClientESP8266.h
@@ -358,11 +358,11 @@ protected:
       waitResponse();
     }
     sendAT(GF("+CIPSTART="), mux, ',', ssl ? GF("\"SSL") : GF("\"TCP"), GF("\",\""), host, GF("\","), port, GF(","), TINY_GSM_TCP_KEEP_ALIVE);
+    // TODO: Check mux
     int rsp = waitResponse(75000L,
                            GFP(GSM_OK),
                            GFP(GSM_ERROR),
                            GF(GSM_NL "ALREADY CONNECT" GSM_NL));
-    waitResponse(100, GF("CONNECT")); // TODO: use mux
     return (1 == rsp);
   }
 
