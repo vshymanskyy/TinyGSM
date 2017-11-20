156f57dae4de17fbe3accd7a67322d3b5acc24bf
ivan28823 <ivan1697@gmail.com>
2017-11-20 01:52:30 -0600

    http constant & prototipes

--- src/TinyGsmClientSIM800.h
+++ src/TinyGsmClientSIM800.h
@@ -39,6 +39,71 @@ enum RegStatus {
   REG_UNKNOWN      = 4,
 };
 
+enum httpTag{
+  CID       = 0,
+  URL       = 1,
+  UA        = 2,
+  PROIP     = 3,
+  PROPORT   = 4,
+  REDIR     = 5,
+  BREAK     = 6,
+  BREAKEND  = 7,
+  TIMEOUT   = 8,
+  CONTENT   = 9,
+  USERDATA  = 10,
+};
+enum httpMethod{
+  GET     = 0,
+  POST    = 1,
+  HEAD    = 2,
+};
+enum httpStatus{
+  CONTINUE                        = 100,
+  SWITCHING_PROTOCOLS             = 101,
+  OK                              = 200,
+  CREATED                         = 201,
+  ACCEPTED                        = 202,
+  NON_AUTHORITATIVE_INFORMATION   = 203,
+  NO_CONTENT                      = 204,
+  RESET_CONTENT                   = 205,
+  PARTIAL_CONTENT                 = 206,
+  MULTIPLE_CHOICES                = 300,
+  MOVED_PERMANENTLY               = 301,
+  FOUND                           = 302,
+  SEE_OTHER                       = 303,
+  NOT_MODIFIED                    = 304,
+  USE_PROXY                       = 305,
+  TEMPORARY_REDIRECT              = 307,
+  BAD_REQUEST                     = 400,
+  UNAUTHORIZED                    = 401,
+  PAYMENT_REQUIRED                = 402,
+  FORBIDDEN                       = 403,
+  NOT_FOUND                       = 404,
+  METHOD_NOT_ALLOWED              = 405, 
+  NOT_ACCEPTABLE                  = 406, 
+  PROXY_AUTHENTICATION_REQUIRED   = 407, 
+  REQUEST_TIME_OUT                = 408, 
+  CONFLICT                        = 409, 
+  GONE                            = 410, 
+  LENGTH_REQUIRED                 = 411, 
+  PRECONDITION_FAILED             = 412, 
+  REQUEST_ENTITY_TOO_LARGE        = 413, 
+  REQUEST_URI_TOO_LARGE           = 414, 
+  UNSUPPORTED_MEDIA_TYPE          = 415, 
+  REQUESTED_RANGE_NOT_SATISFIABLE = 416, 
+  EXPECTATION_FAILED              = 417, 
+  INTERNAL_SERVER_ERROR           = 500, 
+  NOT_IMPLEMENTED                 = 501, 
+  BAD_GATEWAY                     = 502, 
+  SERVICE_UNAVAILABLE             = 503, 
+  GATEWAY_TIME_OUT                = 504, 
+  HTTP_VERSION_NOT_SUPPORTED      = 505, 
+  NOT_HTTP_PDU                    = 600, 
+  NETWORK_ERROR                   = 601, 
+  NO_MEMORY                       = 602, 
+  DNS_ERROR                       = 603, 
+  STACK_BUSY                      = 604, 
+};
 
 class TinyGsmSim800
 {
@@ -735,6 +800,27 @@ public:
     waitResponse();
     return res;
   }
+
+  public:
+  ///HTTP SUPORT
+  bool httpInit(){
+    sendAT();
+  }
+  bool httpUinit(){
+
+  }
+  void httpRequest(httpTag tag,char *value){
+
+  }
+  void httpData(char *data,unsigned long timeout){
+
+  }
+  httpStatus httpMethod(httpMethod method,unsigned long timeout=60000L){
+
+  }
+  String httpRead(){
+
+  }  
 
 protected:
 
