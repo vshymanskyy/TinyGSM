99ddbeae6294f03e3cf6cd755c0f6c3430f548b3
Sara Damiano <sdamiano@stroudcenter.org>
2018-09-07 13:48:31 -0400

    Timeout cleanup

--- src/TinyGsmClientBG96.h
+++ src/TinyGsmClientBG96.h
@@ -698,8 +698,7 @@ public:
     streamWrite(tail...);
   }
 
-  bool streamSkipUntil(char c) {
-    const unsigned long timeout = 1000L;
+  bool streamSkipUntil(char c, const unsigned long timeout = 1000L) {
     unsigned long startMillis = millis();
     while (millis() - startMillis < timeout) {
       while (millis() - startMillis < timeout && !stream.available()) {
