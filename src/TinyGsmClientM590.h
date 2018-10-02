e68ea1db125920121b29c95ecd919438e660a398
Sara Damiano <sdamiano@stroudcenter.org>
2018-10-02 15:32:38 -0400

    Read timeout for modules that immediately spit out data (no buffer)
    They do need a millis() based timeout so that data left in the buffer after the socket closes can still be read in but the maintain doesn't sit and wait forever.

--- src/TinyGsmClientM590.h
+++ src/TinyGsmClientM590.h
@@ -118,7 +118,8 @@ public:
   virtual int read(uint8_t *buf, size_t size) {
     TINY_GSM_YIELD();
     size_t cnt = 0;
-    while (cnt < size && sock_connected) {
+    uint32_t _startMillis = millis();
+    while (cnt < size && millis() - _startMillis < _timeout) {
       size_t chunk = TinyGsmMin(size-cnt, rx.size());
       if (chunk > 0) {
         rx.get(buf, chunk);
