bf9bd360808015fb7b35421eae45526f2635700b
Sara Damiano <sdamiano@stroudcenter.org>
2019-08-28 16:59:35 -0400

    version bump

--- src/TinyGsmCommon.h
+++ src/TinyGsmCommon.h
@@ -10,7 +10,7 @@
 #define TinyGsmCommon_h
 
 // The current library version number
-#define TINYGSM_VERSION "0.9.8"
+#define TINYGSM_VERSION "0.9.9"
 
 #if defined(SPARK) || defined(PARTICLE)
   #include "Particle.h"
