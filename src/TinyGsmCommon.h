17cdde291f90a45c3f556e2e11ffd155f69c9d24
Sara Damiano <sdamiano@stroudcenter.org>
2019-09-09 18:24:24 -0400

    Tiny change to M95 and it works!

--- src/TinyGsmCommon.h
+++ src/TinyGsmCommon.h
@@ -10,7 +10,7 @@
 #define TinyGsmCommon_h
 
 // The current library version number
-#define TINYGSM_VERSION "0.9.16"
+#define TINYGSM_VERSION "0.9.17"
 
 #if defined(SPARK) || defined(PARTICLE)
   #include "Particle.h"
