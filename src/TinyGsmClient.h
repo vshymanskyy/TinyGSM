c29132fdd375852aec855c474f9587727a5e8c18
Sara Damiano <sdamiano@stroudcenter.org>
2024-05-20 12:33:17 -0400

    Remove redundant BG95
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- src/TinyGsmClient.h
+++ src/TinyGsmClient.h
@@ -79,16 +79,11 @@ typedef TinyGsmSaraR5::GsmClientSecureR5 TinyGsmClientSecure;
 typedef TinyGsmM95               TinyGsm;
 typedef TinyGsmM95::GsmClientM95 TinyGsmClient;
 
-#elif defined(TINY_GSM_MODEM_BG96)
+#elif defined(TINY_GSM_MODEM_BG96) || defined(TINY_GSM_MODEM_BG95SSL)
 #include "TinyGsmClientBG96.h"
 typedef TinyGsmBG96                      TinyGsm;
 typedef TinyGsmBG96::GsmClientBG96       TinyGsmClient;
 typedef TinyGsmBG96::GsmClientSecureBG96 TinyGsmClientSecure;
-
-#elif defined(TINY_GSM_MODEM_BG95SSL)
-#include "TinyGsmClientBG95SSL.h"
-typedef TinyGsmBG95                      TinyGsm;
-typedef TinyGsmBG95::GsmClientSecureBG95 TinyGsmClientSecure;
 
 #elif defined(TINY_GSM_MODEM_A6) || defined(TINY_GSM_MODEM_A7)
 #include "TinyGsmClientA6.h"
